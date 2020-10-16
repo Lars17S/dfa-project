f375e38445f74522d1e37bfcc87c10c6f8ee659c
Lars17S <efren.garibaldi@gmail.com>
2020-10-15 20:53:24 -0500

    min dfa

--- DFA/DFA.py
+++ DFA/DFA.py
@@ -45,11 +45,11 @@ dicc=dict(evaluations)
 
 
 #print(dicc)
-print(evaluations)
-print(dicc)
-print(finalStates)
-print(check_dfa('aba'))
-print(symbols)
+# print(evaluations)
+# print(dicc)
+# print(finalStates)
+# print(check_dfa('aba'))
+# print(symbols)
 #print(states,symbols,initialState,finalStates)
 #print(f.read())
 
@@ -62,21 +62,67 @@ def fill_keys():
             if key not in dicc.keys():
                 dicc[key] = 'X'
 fill_keys()
-print(dicc)
+
 
 def find_match():
+
     table = []
     for state in states: # q0
         row_state = []
         row_state.append(state)
+        row_state.append([])
         for lang in symbols:
             key = state + lang 
-            row_state.append(dicc[key])
+            row_state[1].append(dicc[key])
         table.append(row_state)
 
+    equivalents = []
+
     for line in table:
-        print(line, '\n')
+        transitions = line[1]
+        list_eq = []
+        for line_it in table:
+            if line_it[1] == transitions:
+                # print(line_it[0], " is equivalent to ", line[0])
+                list_eq.append(line_it[0])
+        if len(list_eq) > 1:
+            if list_eq not in equivalents:
+                equivalents.append(list_eq)
+
+    print("EQUIVALENTS")
+    print(equivalents)
+    print()
+
+    # q1: qA, q3: qA
+    # q2: qB, q4: qB
+
+    temp_dict = {}
+
+    for index in range(0, len(equivalents)):
+        alias = "Q-" + str(index)
+        for item in equivalents[index]:
+            temp_dict[item] = alias
+
+    new_dict = {}
+
+    for key in dicc.keys():
+        state = key[:-1]
+        if state in temp_dict.keys():
+            new_key = temp_dict[state] + key[len(key) - 1]
+        else: new_key = key
+        new_dict[new_key] = dicc[key]
     
+    for key in new_dict.keys():
+        
+        if new_dict[key] in temp_dict.keys():
+            new_dict[key] = temp_dict[new_dict[key]]
+    
+    print("ORIGINAL: ", dicc)
+    print("NEW: ", new_dict)
+    # dict.get(key)
+    # dict[key] 
+    
+
     
 
     
