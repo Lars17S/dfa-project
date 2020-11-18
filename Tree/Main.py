8c56d3e569a411afca66c018e9fa5d05bfc765aa
Lars17S <efren.garibaldi@gmail.com>
2020-11-17 18:04:13 -0600

    epic tree input

--- Tree/Main.py
+++ Tree/Main.py
@@ -1,14 +1,17 @@
+from anytree import Node, RenderTree
 tree = dict()
 depth_dict= dict()
+node_mapstr = dict()
 nonTerminal = list()
 terminal = list()
 rootKey = ''
 productions = dict()
 
-def top_down_parsing(maxDepth, inputStr):
+def top_down_parsing(inputStr, maxDepth):
     queue = []
     queue.append(rootKey)
     depth_dict[rootKey] = 0
+    node_mapstr[rootKey] = Node(rootKey)
     uwv = ''
     input_found = False
     while queue and not input_found:
@@ -18,7 +21,7 @@ def top_down_parsing(maxDepth, inputStr):
         done = False
         for i in range(len(q)):
             if q[i].isupper():
-                node_part = q.partition(q[i]) # () - () - ()
+                node_part = q.partition(q[i]) # u - w - v
                 break
         while not done and not input_found:
             rules = productions[node_part[1]]
@@ -37,11 +40,9 @@ def top_down_parsing(maxDepth, inputStr):
                     if i < len(inputStr) and u[i] != inputStr[i]:
                         prefix_match = False
                         break
-                if nonTerminal_node and prefix_match:
-                    depth_dict[uwv] = depth_dict[q] + 1
-                    insert_node(q, uwv)
-                    queue.append(uwv)
-                if not nonTerminal_node and prefix_match:
+                if prefix_match:
+                    if nonTerminal_node:
+                        queue.append(uwv)
                     depth_dict[uwv] = depth_dict[q] + 1
                     insert_node(q, uwv)
                 if uwv == inputStr:
@@ -50,14 +51,25 @@ def top_down_parsing(maxDepth, inputStr):
             done = True
     print(tree)
     print(depth_dict)
-    print(input_found)
+    if input_found:
+        print('----------The string is accepted----------')
+    else:
+        print('----------The string is not accepted----------')
+    for pre, fill, node in RenderTree(node_mapstr[rootKey]):
+        print("%s%s" % (pre, node.name))
 
 def insert_node(key, node):
     if key not in tree.keys():
         tree[key] = list()
     tree[key].append(node)
+    node_mapstr[node] = Node(node, node_mapstr[key])
 
-with open("test1.txt") as f:
+test_name = input('Input test file name: ')
+test_name = test_name + '.txt'
+inputStr = input('Input string to test: ')
+maxDepth = int(input('Max depth tree: '))
+
+with open(test_name) as f:
     lines = f.readlines()
     nonTerminal = lines[0].strip().split(',')
     terminal = lines[1].strip().split(',')
@@ -69,5 +81,5 @@ with open("test1.txt") as f:
         if key not in productions.keys():
             productions[key] = list()
         productions[key].append(value)
+top_down_parsing(inputStr, maxDepth)
 
-top_down_parsing(10, 'abbbb')
